53b9c302cdff86b4589a9e11b00701bb6802db6f
Yanwen Huangxu <huangxu.y@husky.neu.edu>
2019-11-13 10:22:21 -0800

    code clean

--- app/src/main/java/yanwenhuangxu/com/bluetooth/PlaceListAdapter.kt
+++ app/src/main/java/yanwenhuangxu/com/bluetooth/PlaceListAdapter.kt
@@ -2,8 +2,6 @@ package yanwenhuangxu.com.bluetooth
 
 import android.bluetooth.BluetoothDevice
 import android.content.Context
-import android.os.Parcel
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,14 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 
 class PlaceListAdapter(private val list:ArrayList<BluetoothDevice>,
-                       private val context: Context) : RecyclerView.Adapter<PlaceListAdapter.ViewHolder>(),
-    Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        TODO("list"),
-        TODO("context")
-    ) {
-    }
+                       private val context: Context) : RecyclerView.Adapter<PlaceListAdapter.ViewHolder>(){
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindItem(list[position])
@@ -35,7 +26,6 @@ class PlaceListAdapter(private val list:ArrayList<BluetoothDevice>,
         return list.size
     }
 
-
     class ViewHolder(itemView:View): RecyclerView.ViewHolder(itemView) {
 
         fun bindItem(bluetoothDevice: BluetoothDevice) {
@@ -43,25 +33,6 @@ class PlaceListAdapter(private val list:ArrayList<BluetoothDevice>,
             val address = itemView.findViewById(R.id.tvDeviceAddress) as TextView
             name.text = bluetoothDevice.name
             address.text = bluetoothDevice.address
-        }
-
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<PlaceListAdapter> {
-        override fun createFromParcel(parcel: Parcel): PlaceListAdapter {
-            return PlaceListAdapter(parcel)
-        }
-
-        override fun newArray(size: Int): Array<PlaceListAdapter?> {
-            return arrayOfNulls(size)
         }
     }
 }
